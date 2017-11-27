211065b3e8594f89bec4987a751f467bab17bf11
caoscott <scottcao@outlook.com>
2017-11-26 23:53:13 -0600

    Added vim installation.

--- install_vim_settings.sh
+++ install_vim_settings.sh
@@ -1,3 +1,4 @@
+apt-get install -y curl vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle && git clone git://github.com/altercation/vim-colors-solarized.git
